0a30f9aae3698fef0c440af7c707cd18e94469a2
Jonas Mueller <jonas@muellers.one>
2016-01-09 12:17:16 +0100

    Clarify verbose output
    Only report rename if it actually happened

--- lib/image_renamer.rb
+++ lib/image_renamer.rb
@@ -13,8 +13,10 @@ class ImageRenamer
       sort_by { |file, date| date }.
       each_with_index do |(file, date), index|
         newname = date.strftime(filename_template(index+1))
-        puts "#{file} -> #{newname}" if verbose
-        FileUtils.mv(file, File.join(File.dirname(file), newname)) unless dry_run
+        unless newname == File.basename(file)
+          puts "#{File.basename(file)} -> #{newname}" if verbose
+          FileUtils.mv(file, File.join(File.dirname(file), newname)) unless dry_run
+        end
       end
   end
 
