e33c6a2fd8014ed47ef5b565782ef638378361b9
Jonas Mueller <jonas@muellers.one>
2016-01-18 20:18:56 +0100

    Nil is not a valid date

--- lib/date_reader.rb
+++ lib/date_reader.rb
@@ -5,7 +5,7 @@ require "date"
 
 class DateReader
   def self.call(file)
-    Exif::Data.new(file).date_time_original
+    Exif::Data.new(file).date_time_original || File.new(file).ctime
   rescue
     puts "Cannot determine exif date for #{file}"
     File.new(file).ctime
