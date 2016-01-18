68f37ab862ab39deb4c7c104b8293da577356d3c
Jonas Mueller <jonas@muellers.one>
2016-01-18 20:14:20 +0100

    Fall back to File#ctime in all cases

--- lib/date_reader.rb
+++ lib/date_reader.rb
@@ -5,8 +5,9 @@ require "date"
 
 class DateReader
   def self.call(file)
-    Exif::Data.new(file).date_time_original || File.new(file).ctime
+    Exif::Data.new(file).date_time_original
   rescue
-    fail "Cannot determine date for #{file}"
+    puts "Cannot determine exif date for #{file}"
+    File.new(file).ctime
   end
 end
